c2af8f50292621451b1657f6a630adde19e95d55
snip <78ku78@gmail.com>
2020-11-20 15:49:03 +0300

    ДЗ5

--- src/main/java/controllers/ProductsController.java
+++ src/main/java/controllers/ProductsController.java
@@ -1,1 +1,58 @@
 package controllers;
+
+
+import com.geekbrains.july.market.julymarket.entities.Product;
+import com.geekbrains.july.market.julymarket.services.ProductsService;
+import com.geekbrains.july.market.julymarket.utils.ProductFilter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/products")
+public class ProductsController {
+    private ProductsService productsService;
+
+    @Autowired
+    public ProductsController(ProductsService productsService) {
+        this.productsService = productsService;
+    }
+
+    @GetMapping
+    public String showAll(Model model, @RequestParam Map<String, String> requestParams) {
+        Integer pageNumber = Integer.parseInt(requestParams.getOrDefault("p", "1"));
+        ProductFilter productFilter = new ProductFilter(requestParams);
+        Page<Product> products = productsService.findAll(productFilter.getSpec(), pageNumber);
+        model.addAttribute("products", products);
+        model.addAttribute("filterDef", productFilter.getFilterDefinition().toString());
+        return "all_products";
+    }
+
+    @GetMapping("/add")
+    public String showAddForm() {
+        return "add_product_form";
+    }
+
+    @PostMapping("/add")
+    public String saveNewProduct(@ModelAttribute Product product) {
+        productsService.saveOrUpdate(product);
+        return "redirect:/products/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        model.addAttribute("product", productsService.findById(id));
+        return "edit_product_form";
+    }
+
+    @PostMapping("/edit")
+    public String modifyProduct(@ModelAttribute Product product) {
+        productsService.saveOrUpdate(product);
+        return "redirect:/products/";
+    }
+}
--- src/main/java/services/ProductsService.java
+++ src/main/java/services/ProductsService.java
@@ -1,2 +1,41 @@
 package services;
 
+import com.geekbrains.july.market.julymarket.entities.Product;
+import com.geekbrains.july.market.julymarket.exceptions.ProductNotFoundException;
+import com.geekbrains.july.market.julymarket.repositories.ProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductsService {
+    private ProductsRepository productsRepository;
+
+    @Autowired
+    public void setProductsRepository(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
+
+    public Product saveOrUpdate(Product product) {
+        return productsRepository.save(product);
+    }
+
+    public Product findById(Long id) {
+        return productsRepository.findById(id).orElseThrow(() -> new ProductNotFoundException("Can't found product with id = " + id));
+    }
+
+    public List<Product> findAll() {
+        return productsRepository.findAll();
+    }
+
+    public Page<Product> findAll(Specification<Product> spec, Integer page) {
+        if (page < 1L) {
+            page = 1;
+        }
+        return productsRepository.findAll(spec, PageRequest.of(page - 1, 10));
+    }
+}
